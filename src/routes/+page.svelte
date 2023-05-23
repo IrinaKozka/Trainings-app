d8b76f81184cd3d4f567a5944b6af91c7ff99336
IrinaKozka <109234531+IrinaKozka@users.noreply.github.com>
2023-05-23 05:04:17 +0200

    add home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,55 @@
+<div class="container">
+<img src="/code-big.jpg" alt="programming, code">
+</div>
 
+               <h2><br>WELCOME IN TRAININGS </h2>
+               <div class="text">
+<p>Intensive programming courses taught by industry experts. We teach online live with a trainer in group.
+           <br><br> LEARNING AT HOME<br>
+Courses are organized on a daily and weekend basis. Between blocks there is a break - needed for home training.  We have a wide selection of courses.  Everyone will find something for themselves.
+ Courses are designed to cater to various skill levels, from beginners to advanced programmers. Whether you are just starting your coding journey or looking to enhance your existing skills, our courses provide a comprehensive learning experience.<br><br>
+
+Our curriculum covers a range of popular programming languages and frameworks, including but not limited to Python, JavaScript, Java, C++, and HTML/CSS. You will have the opportunity to dive deep into these technologies and gain practical hands-on experience through coding exercises and projects.<br><br>
+
+Furthermore, our courses are regularly updated to stay current with the latest industry trends and best practices. This ensures that you receive the most relevant and up-to-date knowledge in the rapidly evolving field of programming.<br><br>
+
+At Trainings, we believe in fostering a supportive and collaborative learning environment. You will have the chance to interact with fellow participants, exchange ideas, and work on team projects. This not only enhances your technical skills but also helps you develop essential teamwork and communication abilities.
+
+Upon completion of a course, you will receive a certificate of achievement, recognizing your dedication and successful completion of the program. This certificate can be a valuable addition to your resume and can help you stand out in the competitive job market.
+
+So why wait? Take the first step towards mastering programming and join us at Trainings for an enriching and rewarding learning experience. Let's unlock your coding potential together!
+
+Remember, learning programming is a journey, and we are here to support you every step of the way.
+</div>
+
+<style>
+    h2 {
+display: flex;
+justify-content: center;
+    }
+
+    .text{
+width: 100%;
+height: 80%;
+font-size: 0.8rem;
+display: flex;
+    }
+
+    p {
+ font-size: 1rem;
+ font-weight: 250;
+ line-height: 24px;
+ letter-spacing: 0em;
+ text-align: center;
+     }
+
+     .container {
+ display: flexbox;
+ width: 100%;
+height: 20rem;
+background-image: url(code-big.jpg);
+background-size: cover;
+background-position: center;
+ background-repeat: no-repeat;
+    }  
+</style>
